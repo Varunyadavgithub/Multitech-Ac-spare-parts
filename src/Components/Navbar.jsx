1ba0d0261c250d1a94155eab4aea4fc9b678de31
Varun Yadav <vy056038@gmail.com>
2024-09-10 12:26:04 +0530

    navbar done with logo image

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaBars, FaTimes } from 'react-icons/fa';
+import logo from "../assets/logo.png";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,24 +13,36 @@ function Navbar() {
   return (
     <nav className="bg-black p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-2xl font-bold">
-          <Link to="/">MultiTech AC Spare Parts</Link>
+        <div className="flex items-center">
+          <Link to="/">
+            <img 
+              src={logo}
+              alt="MultiTech AC Spare Parts" 
+              className="h-20 w-auto mr-4" 
+            />
+          </Link>
         </div>
 
-      
+        <div className="hidden md:flex space-x-6">
+          <Link to="/" className="text-white hover:text-yellow-400">Home</Link>
+          <Link to="/products" className="text-white hover:text-yellow-400">Products</Link>
+          <Link to="/about" className="text-white hover:text-yellow-400">About Us</Link>
+          <Link to="/contact" className="text-white hover:text-yellow-400">Contact</Link>
+        </div>
+
         <div className="md:hidden">
           <button
             onClick={toggleNavbar}
             type="button"
             className="text-white focus:outline-none"
           >
-            {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />} {/* Toggle between icons */}
+            {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
           </button>
         </div>
       </div>
 
-      <div className={`mt-2 ${isOpen ? 'block' : 'hidden'} md:block`}>
-        <ul className="flex flex-col md:flex-row md:space-x-6">
+      <div className={`md:hidden mt-2 ${isOpen ? 'block' : 'hidden'}`}>
+        <ul className="flex flex-col space-y-2">
           <li>
             <Link to="/" className="text-white hover:text-yellow-400 block px-3 py-2 rounded-md">Home</Link>
           </li>
