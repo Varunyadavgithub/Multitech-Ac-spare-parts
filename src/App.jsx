5536060add92507a9b84b4701a4116059416a278
Varun Yadav <vy056038@gmail.com>
2024-09-10 11:52:29 +0530

    Navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import Products from "./Pages/Products";
+import Aboutus from "./Pages/Aboutus";
+import Contact from "./Pages/Contact";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<Aboutus />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
-const Navbar = () => {
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>
-      <nav>
-        
-      </nav>
-    </div>
-  )
+    <nav className="bg-black p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-2xl font-bold">
+          <Link to="/">MultiTech AC Spare Parts</Link>
+        </div>
+
+      
+        <div className="md:hidden">
+          <button
+            onClick={toggleNavbar}
+            type="button"
+            className="text-white focus:outline-none"
+          >
+            {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />} {/* Toggle between icons */}
+          </button>
+        </div>
+      </div>
+
+      <div className={`mt-2 ${isOpen ? 'block' : 'hidden'} md:block`}>
+        <ul className="flex flex-col md:flex-row md:space-x-6">
+          <li>
+            <Link to="/" className="text-white hover:text-yellow-400 block px-3 py-2 rounded-md">Home</Link>
+          </li>
+          <li>
+            <Link to="/products" className="text-white hover:text-yellow-400 block px-3 py-2 rounded-md">Products</Link>
+          </li>
+          <li>
+            <Link to="/about" className="text-white hover:text-yellow-400 block px-3 py-2 rounded-md">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact" className="text-white hover:text-yellow-400 block px-3 py-2 rounded-md">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
